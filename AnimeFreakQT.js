a2f20bef7b2bc0e0e5884f0030be5bce392b2995
Dan <dannyprud@yahoo.com>
2022-09-03 17:40:47 -0500

    Created fucntion to send a message but it will not work when called from axios .then(), searching for solution

--- AnimeFreakQT.js
+++ AnimeFreakQT.js
@@ -1,6 +1,13 @@
-const { Client, GatewayIntentBits, Intents } = require("discord.js");
+const {
+  Client,
+  GatewayIntentBits,
+  Intents,
+  TextChannel,
+} = require("discord.js");
 const axios = require("axios");
 const dotenv = require("dotenv");
+const { send } = require("process");
+const { time } = require("console");
 
 dotenv.config();
 
@@ -15,16 +22,53 @@ const client = new Client({
   ],
 });
 
+//Send a message
+const sendMessage = (message, channel = "232890995597901824") => {
+  client.on("ready", (client) => {
+    client.channels.cache.get(channel).send(message);
+  });
+};
+
+const log = (data) => {
+  console.log(data + "hafdha;lksdfajhdf");
+};
+
+//call an API
+const getJoke = async () => {
+  await axios
+    .get("https://api.chucknorris.io/jokes/random")
+    .then((res) => {
+      console.log(res.data.value);
+      sendMessage(res.data.value);
+      return res.data.value;
+    })
+    .catch((error) => {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log("Error", error.message);
+      }
+      console.log(error.config);
+    });
+};
+
 client.once("ready", () => {
   console.log(`Bot ${client.user.tag} ready!`);
+  const joke = client.channels.cache.get("232890995597901824");
+  joke.send(getJoke());
 });
 
 client.on("messageCreate", (message) => {
-  if (message.content == "test" && message.author.id !== client.user.id) {
-    message.reply("pong");
+  if (!message.author.bot) {
+    message.reply(`Hmmmm, ${message.content}, very interesting`);
     //console.log(message.author)
     //console.log(client.user)
   }
 });
 
+//sendMessage("fasdf");
 client.login(token);
